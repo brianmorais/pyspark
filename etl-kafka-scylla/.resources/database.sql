000e8df4710f51f859a2b921d869e20a06a789e3
Brian Leonardo de Morais <b.leonardo.de.morais@avanade.com>
2025-08-10 08:17:04 -0300

    Adicionado script para criação de banco e renomeado pasta de resources

---
+++ etl-kafka-scylla/.resources/database.sql
@@ -0,0 +1,10 @@
+CREATE KEYSPACE IF NOT EXISTS etl 
+WITH replication = {'class': 'NetworkTopologyStrategy', 'datacenter1': 1 };
+
+CREATE TABLE etl.persons (
+    name text,
+    age int,
+    city text,
+    age_in_months int,
+    PRIMARY KEY (name, age, city)
+);
