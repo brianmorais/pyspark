ea5f672bca9f4a618673cd359d598c1184384338
Brian Leonardo de Morais <b.leonardo.morais@gmail.com>
2025-08-16 18:12:04 -0300

    Adicionado devcontainer

--- etl-kafka-scylla/run.py
+++ etl-kafka-scylla/run.py
@@ -2,10 +2,10 @@ from os import environ
 
 environ["KAFKA_PERSONS_TOPIC"] = "persons"
 environ["KAFKA_PROCESSED_TOPIC"] = "processed"
-environ["KAFKA_BOOTSTRAP_SERVERS"] = "localhost:9092"
+environ["KAFKA_BOOTSTRAP_SERVERS"] = "kafka:9094"
 environ["SCYLLA_KEYSPACE"] = "etl"
 environ["SCYLLA_TABLE"] = "persons"
-environ["SCYLLA_HOST"] = "localhost"
+environ["SCYLLA_HOST"] = "scylla"
 environ["SCYLLA_PORT"] = "9042"
 
 from src.application.main import main
